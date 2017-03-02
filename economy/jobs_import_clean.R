8954cc830795ec757adf9a1a863de2886c78a91a
Michael Reilly <mreilly@mtc.ca.gov>
2017-03-02 13:32:06 -0800

    more

--- economy/jobs_import_clean.R
+++ economy/jobs_import_clean.R
@@ -4,3 +4,6 @@ est15_esri_raw = read.csv("./rawdata/est15_esri_raw.csv", header = TRUE)
 naics_recode = read.csv("naics_recode.csv", header = TRUE)
 
 (est15_codes <- left_join(est15_esri_raw, naics_recode))
+
+est15_summ <- ddply(est15_codes, c("maz", "shcat"), summarise,
+               emp    = sum(empnum)

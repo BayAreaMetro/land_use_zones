cd311f3a25bd15e7a07f725b7d25e24b9f472f02
Michael Reilly <mreilly@mtc.ca.gov>
2017-03-02 14:14:42 -0800

    more

--- economy/jobs_import_clean.R
+++ economy/jobs_import_clean.R
@@ -1,4 +1,5 @@
 install.packages("dplyr")
+library(dplyr)
 library(readr)
 
 est15_esri_raw <- read_csv("/Volumes/osaka/w/land_use_zones/economy/rawdata/est15_esri_raw.csv")
@@ -7,6 +8,7 @@ naics_recode <- read_csv("/Volumes/osaka/w/land_use_zones/economy/naics_recode.c
 # make 6-digit NAICS
 est15_esri_raw$naicssix = est15_esri_raw$NAICS %/% 100
 
+est15_codes <- left_join(est15_esri_raw, naics_recode, by = "naicssix")
 (est15_codes <- left_join(est15_esri_raw, naics_recode))
 
 est15_summ <- ddply(est15_codes, c("maz", "shcat"), summarise,

115f13242bc3e5ac5cf48aff91f04825bf726b32
Michael Reilly <mreilly@mtc.ca.gov>
2017-03-03 12:21:43 -0800

    more

--- economy/jobs_import_clean.R
+++ economy/jobs_import_clean.R
@@ -6,6 +6,7 @@ library(readr)
 
 est15_esri_raw <- read_csv("/Volumes/osaka/w/land_use_zones/economy/rawdata/est15_esri_raw.csv")
 naics_recode <- read_csv("/Volumes/osaka/w/land_use_zones/economy/naics_recode.csv")
+geofile <- read_csv("/Volumes/osaka/w/land_use_zones/geofile.csv")
 
 # make 6-digit NAICS
 est15_esri_raw$naicssix = est15_esri_raw$NAICS %/% 100
@@ -14,25 +15,129 @@ est15_codes <- left_join(est15_esri_raw, naics_recode, by = "naicssix")
 
 # drop missing
 
-  sumby <- est15_codes %>%
-  group_by(MAZ_ORIGINAL, shcat) %>%
+  sum <- est15_codes %>%
+  group_by(MAZ, shcat) %>%
   tally(EMPNUM)
 
 summary(sum)
-# write_csv(sum, "/Volumes/osaka/w/land_use_zones/economy/rawdata/emp15_maz.csv" )
 
 e_ag <- filter(sum, shcat == 'ag')
 e_ag <- transmute(e_ag,
-                    emp_ag = n)
+                    e_ag = n)
 e_natres <- filter(sum, shcat == 'natres')
 e_natres <- transmute(e_natres,
-                  emp_natres = n)
+                  e_natres = n)
 e_util <- filter(sum, shcat == 'util')
 e_util <- transmute(e_util,
-          emp_util = n)
+          e_util = n)
+e_constr <- filter(sum, shcat == 'constr')
+e_constr <- transmute(e_constr,
+                    e_constr = n)
+e_man_lgt <- filter(sum, shcat == 'man_lgt')
+e_man_lgt <- transmute(e_man_lgt,
+                    e_man_lgt = n)
+e_man_hvy <- filter(sum, shcat == 'man_hvy')
+e_man_hvy <- transmute(e_man_hvy,
+                    e_man_hvy = n)
+e_man_bio <- filter(sum, shcat == 'man_bio')
+e_man_bio <- transmute(e_man_bio,
+                    e_man_bio = n)
+e_man_tech <- filter(sum, shcat == 'man_tech')
+e_man_tech <- transmute(e_man_tech,
+                    e_man_tech = n)
+e_logis <- filter(sum, shcat == 'logis')
+e_logis <- transmute(e_logis,
+                    e_logis = n)
+e_ret_reg <- filter(sum, shcat == 'ret_reg')
+e_ret_reg <- transmute(e_ret_reg,
+                    e_ret_reg = n)
+e_ret_loc <- filter(sum, shcat == 'ret_loc')
+e_ret_loc <- transmute(e_ret_loc,
+                    e_ret_loc = n)
+e_transp <- filter(sum, shcat == 'transp')
+e_transp <- transmute(e_transp,
+                    e_transp = n)
+e_info <- filter(sum, shcat == 'info')
+e_info <- transmute(e_info,
+                    e_info = n)
+e_fire <- filter(sum, shcat == 'fire')
+e_fire <- transmute(e_fire,
+                    e_fire = n)
+e_serv_pers <- filter(sum, shcat == 'serv_pers')
+e_serv_pers <- transmute(e_serv_pers,
+                    e_serv_pers = n)
+e_lease <- filter(sum, shcat == 'lease')
+e_lease <- transmute(e_lease,
+                    e_lease = n)
+e_prof <- filter(sum, shcat == 'prof')
+e_prof <- transmute(e_prof,
+                    e_prof = n)
+e_serv_bus <- filter(sum, shcat == 'serv_bus')
+e_serv_bus <- transmute(e_serv_bus,
+                    e_serv_bus = n)
+e_ed_k12 <- filter(sum, shcat == 'ed_k12')
+e_ed_k12 <- transmute(e_ed_k12,
+                    e_ed_k12 = n)
+e_ed_high <- filter(sum, shcat == 'ed_high')
+e_ed_high <- transmute(e_ed_high,
+                    e_ed_high = n)
+e_ed_other <- filter(sum, shcat == 'ed_other')
+e_ed_other <- transmute(e_ed_other,
+                    e_ed_other = n)
+e_health <- filter(sum, shcat == 'health')
+e_health <- transmute(e_health,
+                    e_health = n)
+e_serv_soc <- filter(sum, shcat == 'serv_soc')
+e_serv_soc <- transmute(e_serv_soc,
+                    e_serv_soc = n)
+e_art_rec <- filter(sum, shcat == 'art_rec')
+e_art_rec <- transmute(e_art_rec,
+                    e_art_rec = n)
+e_hotel <- filter(sum, shcat == 'hotel')
+e_hotel <- transmute(e_hotel,
+                    e_hotel = n)
+e_eat <- filter(sum, shcat == 'eat')
+e_eat <- transmute(e_eat,
+                    e_eat = n)
+e_gov <- filter(sum, shcat == 'gov')
+e_gov <- transmute(e_gov,
+                    e_gov = n)
+e_mis <- filter(sum, shcat == 'mis')
+e_mis <- transmute(e_mis,
+                    e_mis = n)
 
-est15_maz <- full_join(e_ag, e_natres, by = "MAZ_ORIGINAL")
+mazlist <- select(geofile, MAZ)
 
+est15_maz <- full_join(mazlist, e_ag, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_natres, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_util, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_constr, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_man_lgt, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_man_hvy, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_man_bio, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_man_tech, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_logis, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_ret_reg, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_ret_loc, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_transp, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_info, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_fire, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_serv_pers, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_lease, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_prof, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_serv_bus, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_ed_k12, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_ed_high, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_ed_other, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_health, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_serv_soc, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_art_rec, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_hotel, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_eat, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_gov, by = "MAZ")
+est15_maz <- full_join(est15_maz, e_mis, by = "MAZ")
+
+write_csv(est15_maz, "/Volumes/osaka/w/land_use_zones/economy/rawdata/emp15_maz.csv" )
 
 
 
@@ -41,19 +146,7 @@ est15_maz <- full_join(e_ag, e_natres, by = "MAZ_ORIGINAL")
 
 # arrange to sort
 
-
-
-#########
-grouped <- group_by(est15_codes$MAZ_ORIGINAL, est15_codes$shcat)
-
-
-
-est15_summ <- ddply(est15_codes, c("maz", "shcat"), function(x) c(count=nrow(x)), emp = sum(empnum))
-               
-              
-               
-
-                                                                  
+                        
                                                                   
 
 #adjust
